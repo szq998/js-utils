0d3aa0fdddf56a114b34f9f550ea980a9731d793
szq <szq001@foxmail.com>
2021-05-05 18:25:48 +0800

    bug fix and improvements

--- work-with-timeout-and-retry.js
+++ work-with-timeout-and-retry.js
@@ -14,16 +14,16 @@ const makePromiseInterruptible = (p) => {
  * @param workWithAbortCleaner {Function} A function that either return a promise or an array contains a promise
  *                                        and a clean routine which will be called when current
  *                                        retry failed/aborted.
- * @param args {Array} Arguments for the workWithAbortCleaner function.
  * @param maxTime {Number} Maximum time for every retry.
  * @param maxRetry {Number} Maximum times of retry.
+ * @param args {Array} Arguments for the workWithAbortCleaner function.
  * @return {any} determined by workWithAbortCleaner
  */
 function workWithTimeoutAndRetry(
     workWithAbortCleaner,
-    args,
     maxTime,
-    maxRetry
+    maxRetry,
+    ...args
 ) {
     return new Promise((resolve, reject) => {
         const errors = [];
@@ -48,32 +48,42 @@ function workWithTimeoutAndRetry(
             // make work promise interruptible
             const [
                 interruptibleWorkPromise,
-                interrupt,
+                interruptWorkPromise,
             ] = makePromiseInterruptible(workPromise);
             // set retry routine when time out
-            const onNeedRetry = () => {
+            const retry = () => {
+                // reject the time out work promise
+                interruptWorkPromise(
+                    maxRetry > 1
+                        ? new TimeoutError(
+                              `Timeout for the ${currRetry + 1}${
+                                  currRetry === 0
+                                      ? 'st'
+                                      : currRetry === 1
+                                      ? 'rd'
+                                      : 'th'
+                              } time`
+                          )
+                        : new TimeoutError()
+                );
                 // run clean routine if provided
                 clean?.();
-                // reject then time out work promise
-                interrupt(
-                    new TimeoutError(
-                        `Work is timeout for the ${currRetry + 1} time`
-                    )
-                );
                 if (currRetry < maxRetry) {
                     // retry
                     work(currRetry + 1);
                 } else {
                     // end retry
                     reject(
-                        new AggregateError(
-                            errors,
-                            `Retry exceeds ${maxRetry} times`
-                        )
+                        maxRetry > 1
+                            ? new AggregateError(
+                                  errors,
+                                  `Retry exceeds ${maxRetry} times`
+                              )
+                            : errors[0]
                     );
                 }
             };
-            const retryTimer = setTimeout(onNeedRetry, maxTime);
+            const retryTimer = setTimeout(retry, maxTime);
             // do the work
             try {
                 resolve(await interruptibleWorkPromise);
@@ -84,7 +94,7 @@ function workWithTimeoutAndRetry(
                 // work promise is not rejected by the retry routine
                 clearTimeout(retryTimer);
                 // retry immediately
-                onNeedRetry();
+                retry();
             }
         })();
     });
